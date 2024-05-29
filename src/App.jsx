17a109df76e620868d813ad2122140b839b8c15c
Ferchox <fernandotriguero123@gmail.com>
2024-05-29 10:15:51 -0400

    Componente 6 y 9 en carpetas

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
 import React, { useState } from "react";
 import "./App.css";
+import Componente6 from "./components/Componente6.jsx";
+import Componente9 from "./components/Componente9.jsx";
 
 function App() {
   const [value, setValue] = useState("");
 
   return (
-    <div className="container">
-      <h1>Título de meta</h1>
-      <input type="text" />
-      <h1>Seleccionar fecha límite:</h1>
-      <input
-        type="date"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        min={new Date().toISOString().split("T")[0]}
-        max={
-          new Date(new Date().setFullYear(new Date().getFullYear() + 1))
-            .toISOString()
-            .split("T")[0]
-        }
-      />
-      <button>Aceptar</button>
-    </div>
+    <>
+      <Componente6 />
+      <Componente9 />
+    </>
   );
 }
 
---
+++ src/components/Componente9.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./Componente9.css";
+
+function Componente9() {
+  const [metas, setMetas] = useState([
+    { objetivo: "Meta 1", fechaLimite: "20/04", realizada: true },
+    { objetivo: "Meta 2", fechaLimite: "20/06", realizada: false },
+  ]);
+
+  const handleCheck = (index) => {
+    const updatedMetas = [...metas];
+    updatedMetas[index].realizada = !updatedMetas[index].realizada;
+    setMetas(updatedMetas);
+  };
+
+  return (
+    <>
+      <div className="header">Metas</div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Objetivo</th>
+            <th>Fecha límite</th>
+            <th>Realizada</th>
+          </tr>
+        </thead>
+        <tbody>
+          {metas.map((meta, index) => (
+            <tr key={index}>
+              <td>{meta.objetivo}</td>
+              <td>{meta.fechaLimite}</td>
+              <td>
+                <input
+                  type="checkbox"
+                  checked={meta.realizada}
+                  onChange={() => handleCheck(index)}
+                />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button className="accept-button">Aceptar</button>
+    </>
+  );
+}
+
+export default Componente9;
