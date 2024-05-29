644edfa27348977e23cab590a152493e8c67add6
ALEJOO <zallesrene@gmail.com>
2024-05-29 10:09:14 -0400

    agregando carpetas

---
+++ src/components/miPerfil/InformacionPerfil.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const InformacionPerfil = () => {
+    return (
+        <div>
+            <img src="" alt="" />
+            <label htmlFor="">Nombre cliente</label>
+            <label htmlFor="">Edad</label>
+            <label htmlFor="">Sexo</label>
+        </div>
+    )
+}
+
+export default InformacionPerfil
