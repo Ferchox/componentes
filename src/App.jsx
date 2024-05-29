f022d2fd5719eac9ad66892a8b0a4a7f0082c309
Ferchox <fernandotriguero123@gmail.com>
2024-05-29 09:23:29 -0400

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [value, setValue] = useState("");
+
+  return (
+    <div className="container">
+      <h1>Título de meta</h1>
+      <input type="text" />
+      <h1>Seleccionar fecha límite:</h1>
+      <input
+        type="date"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        min={new Date().toISOString().split("T")[0]}
+        max={
+          new Date(new Date().setFullYear(new Date().getFullYear() + 1))
+            .toISOString()
+            .split("T")[0]
+        }
+      />
+      <button>Aceptar</button>
+    </div>
+  );
+}
+
+export default App;
