b075b79e5884eb50b2ccd9cf78e19c89cbbbd83b
ALEJOO <zallesrene@gmail.com>
2024-05-29 10:43:01 -0400

    cambiando el nombre de la carpeta de perfilCliente

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Componente6 from "./components/Componente6.jsx";
 import Componente9 from "./components/Componente9.jsx";
-import InformacionPerfil from "./components/miPerfil/InformacionPerfil";
+import InformacionPerfil from "./components/perfilCliente/InformacionPerfil.jsx";
 
 function App() {
 
