e842ef993874abb7d6e1d0548592ba5d4d847cb4
yerain <nahuelcuestas4@gmailcom>
2024-05-29 20:12:01 -0400

    componente5

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import "./App.css";
 import Componente3 from "./components/Componente3.jsx";
+import Componente5 from "./components/Componente5.jsx";
 import Componente6 from "./components/Componente6.jsx";
 import Componente9 from "./components/Componente9.jsx";
 import InformacionPerfil from "./components/perfilCliente/InformacionPerfil.jsx";
-
 
 function App() {
 
   return (
     <>
       <Componente3 />
+      <Componente5 />
       <Componente6 />
       <Componente9 />
       <InformacionPerfil />
