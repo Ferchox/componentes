7dbfd57122e8bbd50b3c42b76a8f36567c368a3a
jdanigh03 <dani151203gh@gmail.com>
2024-05-30 20:54:09 -0400

    resolviendo conflictos del componente 12

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import Componente5 from "./components/Componente5.jsx";
 import Componente6 from "./components/Componente6.jsx";
 import Componente9 from "./components/Componente9.jsx";
 import InformacionPerfil from "./components/perfilCliente/InformacionPerfil.jsx";
-
+import ComponenteOcho from "./components/ComponenteOcho/ComponenteOcho.jsx";
+import DropdownPerfil from "./components/DropdownPerfil/DropdownPerfil.jsx";
+import Componente12 from "./components/Componente12.jsx";
 function App() {
 
   return (
