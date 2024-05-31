777bc35697db33e39e338088d882eb1f7f6b8091
jdanigh03 <dani151203gh@gmail.com>
2024-05-30 20:36:17 -0400

    añadiendo dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Componente3 from "./components/Componente3.jsx";
 import Componente5 from "./components/Componente5.jsx";
 import Componente6 from "./components/Componente6.jsx";
 import Componente9 from "./components/Componente9.jsx";
-import ComponenteOcho from "./components/ComponenteOcho.jsx";
+import ComponenteOcho from "./components/ComponenteOcho/ComponenteOcho.jsx";
+import DropdownPerfil from "./components/DropdownPerfil/DropdownPerfil.jsx";
 import InformacionPerfil from "./components/perfilCliente/InformacionPerfil.jsx";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Componente9 />
       <InformacionPerfil />
       <ComponenteOcho/>
+      <DropdownPerfil/>
     </>
   );
 }
---
+++ src/components/DropdownPerfil/DropdownPerfil.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fontawesome/react-fontawesome';
+import { faUser, faCheck, faChartBar, faDoorOpen } from '@fortawesome/free-solid-svg-icons';
+
+const DropdownPerfil = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="dropdown">
+      <button onClick={toggleDropdown} className="dropdown-toggle">
+        Usuario
+      </button>
+      {isOpen && (
+        <ul className="dropdown-menu">
+          <li><FontAwesomeIcon icon={faUser} /> Ver mi perfil</li>
+          <li><FontAwesomeIcon icon={faCheck} /> Metas</li>
+          <li><FontAwesomeIcon icon={faChartBar} /> Ver progreso</li>
+          <li><FontAwesomeIcon icon={faDoorOpen} /> Cerrar sesión</li>
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default DropdownPerfil;
