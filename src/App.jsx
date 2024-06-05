b16358ccbb8caad55e946f5524a3cfa3e109ee78
jdanigh03 <dani151203gh@gmail.com>
2024-06-05 19:34:03 -0400

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import "./App.css";
 import Perfil from "./components/chat/pages/Chat.jsx"
+import Chat from './components/chat/pages/Chat.jsx';
 
 function App() {
   return (
     <>
-      <Perfil />
+      <Perfil/>
     </>
   );
 }
