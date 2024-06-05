980c0f65aef9616ac6868e1683494e1486409fde
Ferchox <fernandotriguero123@gmail.com>
2024-06-05 15:09:19 -0400

    importacion de chat corregida

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./App.css";
-import Perfil from "./components/perfilCliente/pages/Perfil.jsx"
+import Perfil from "./components/chat/pages/Chat.jsx"
 
 function App() {
   return (
