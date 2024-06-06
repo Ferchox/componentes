ec184c882bdd45ff6b6f50d87fc6d33e619e7156
ALEJOO <zallesrene@gmail.com>
2024-06-06 11:47:49 -0400

    arreglando css de crear metas

--- src/components/crearMetas/moleculas/InputFecha.jsx
+++ src/components/crearMetas/moleculas/InputFecha.jsx
@@ -5,17 +5,9 @@ function InputFecha() {
   const [value, setValue] = useState("");
 
   return (
-    <input className="entrada-fecha"
-      type="date"
-      value={value}
-      onChange={(e) => setValue(e.target.value)}
-      min={new Date().toISOString().split("T")[0]}
-      max={
-        new Date(new Date().setFullYear(new Date().getFullYear() + 1))
-          .toISOString()
-          .split("T")[0]
-      }
-    />
+    <div className="contenedor-entrada-fecha">
+      <input className='campo-texto-fecha' type="date" value={value} onChange={(e) => setValue(e.target.value)} min={new Date().toISOString().split("T")[0]} max={new Date(new Date().setFullYear(new Date().getFullYear() + 1)).toISOString().split("T")[0]} />
+    </div>
   );
 }
 
