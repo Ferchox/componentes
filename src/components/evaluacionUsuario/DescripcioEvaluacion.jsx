f0e72b10658bc347ca75023f8d902a8cf33d9145
KristyRojas <kristinarojas124@gmail.com>
2024-06-06 05:51:11 -0400

    añadir descripcion

---
+++ src/components/evaluacionUsuario/DescripcioEvaluacion.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './DescripcionEvaluacionUsuario.css';
+
+function DescripcionEvaluacionUsuario() {
+
+  return (
+    <div className="App">
+      <div className="titulo"><h3>Evolución</h3></div>
+      <div className="tit">
+      <h5>Hola cliente, esta es tu evolución en el gimnasio. No te olvides que puedes seleccionar el musculo al que quieras ver su progreso</h5>
+      </div>
+    </div>
+  );
+}
+
+export default DescripcionEvaluacionUsuario;
