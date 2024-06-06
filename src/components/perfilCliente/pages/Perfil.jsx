6bc921b0ab10e84a2e5f41f32006353cbd9803ac
ALEJOO <zallesrene@gmail.com>
2024-06-05 20:41:33 -0400

    arreglando estilo

--- src/components/perfilCliente/pages/Perfil.jsx
+++ src/components/perfilCliente/pages/Perfil.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import InformacionPerfil from '../organismos/InformacionPerfil'
-import BarraNavegacion from '../../general/organismos/BarraNavegacion'
-import Cabecera from '../../general/organismos/Cabecera'
-import PiePagina from '../../general/organismos/PiePagina'
-import EtiquetaPagina from '../moleculas/EtiquetaPagina'
+import React from 'react';
+import InformacionPerfil from '../organismos/InformacionPerfil';
+import BarraNavegacion from '../../general/organismos/BarraNavegacion';
+import Cabecera from '../../general/organismos/Cabecera';
+import PiePagina from '../../general/organismos/PiePagina';
+import EtiquetaPagina from '../moleculas/EtiquetaPagina';
+import './Perfil.css';
 
 const Perfil = () => {
   return (
     <>
-      <Cabecera />
-      <BarraNavegacion />
-      <EtiquetaPagina />
-      <InformacionPerfil />
-      <PiePagina />
+      <div className="perfil-container">
+        <Cabecera />
+        <BarraNavegacion />
+        <EtiquetaPagina />
+        <InformacionPerfil />
+        <PiePagina />
+      </div>
     </>
-  )
+  );
 }
 
-export default Perfil
+export default Perfil;
