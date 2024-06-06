302acf556e525cd6c1fb181a133f64d15b64e699
ALEJOO <zallesrene@gmail.com>
2024-06-06 11:58:57 -0400

    prueba

--- src/components/crearMetas/moleculas/InputFecha.jsx
+++ src/components/crearMetas/moleculas/InputFecha.jsx
@@ -5,9 +5,17 @@ function InputFecha() {
   const [value, setValue] = useState("");
 
   return (
-    <div className="contenedor-entrada-fecha">
-      <input className='campo-texto-fecha' type="date" value={value} onChange={(e) => setValue(e.target.value)} min={new Date().toISOString().split("T")[0]} max={new Date(new Date().setFullYear(new Date().getFullYear() + 1)).toISOString().split("T")[0]} />
-    </div>
+    <input className="entrada-fecha"
+      type="date"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      min={new Date().toISOString().split("T")[0]}
+      max={
+        new Date(new Date().setFullYear(new Date().getFullYear() + 1))
+          .toISOString()
+          .split("T")[0]
+      }
+    />
   );
 }
 
