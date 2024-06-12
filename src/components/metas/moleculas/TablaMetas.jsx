407ef3731c5ac43ef946050940eea0affaf04baf
ALEJOO <zallesrene@gmail.com>
2024-06-11 20:57:41 -0400

    agregando caso de no haber datos

--- src/components/metas/moleculas/TablaMetas.jsx
+++ src/components/metas/moleculas/TablaMetas.jsx
@@ -21,58 +21,64 @@ function TablaMetas({ metas, handleCheck, handleDelete, handleEdit }) {
   return (
     <div className="tabla-container">
       <div className="header">Metas</div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Objetivo</th>
-            <th>Fecha límite</th>
-            <th>Realizada</th>
-            <th>Acciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {metas.map((meta) => (
-            <tr key={meta.id}>
-              {editMode === meta.id ? (
-                <>
-                  <td>
-                    <input
-                      type="text"
-                      value={editedMeta}
-                      onChange={(e) => setEditedMeta(e.target.value)}
-                    />
-                  </td>
-                  <td>
-                    <input
-                      type="date"
-                      value={editedFechaLimite}
-                      onChange={(e) => setEditedFechaLimite(e.target.value)}
-                    />
-                  </td>
-                  <td>
-                    <button onClick={() => saveEdit(meta.id)}>Guardar</button>
-                  </td>
-                </>
-              ) : (
-                <>
-                  <td>{meta.meta}</td>
-                  <td>{new Date(meta.fechaLimite).toLocaleDateString()}</td>
-                  <td>
-                    <Checkbox
-                      checked={meta.realizado}
-                      onChange={() => handleCheck(meta.id, !meta.realizado)}
-                    />
-                  </td>
-                </>
-              )}
-              <td>
-                <button onClick={() => handleDelete(meta.id)}>Eliminar</button>
-                <button onClick={() => startEdit(meta)}>Editar</button>
-              </td>
+      {metas.length > 0 ? (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Objetivo</th>
+              <th>Fecha límite</th>
+              <th>Realizada</th>
+              <th>Acciones</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {metas.map((meta) => (
+              <tr key={meta.id}>
+                {editMode === meta.id ? (
+                  <>
+                    <td>
+                      <input
+                        type="text"
+                        value={editedMeta}
+                        onChange={(e) => setEditedMeta(e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <input
+                        type="date"
+                        value={editedFechaLimite}
+                        onChange={(e) => setEditedFechaLimite(e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <button onClick={() => saveEdit(meta.id)}>Guardar</button>
+                    </td>
+                  </>
+                ) : (
+                  <>
+                    <td>{meta.meta}</td>
+                    <td>{new Date(meta.fechaLimite).toLocaleDateString()}</td>
+                    <td>
+                      <Checkbox
+                        checked={meta.realizado}
+                        onChange={() => handleCheck(meta.id, !meta.realizado)}
+                      />
+                    </td>
+                  </>
+                )}
+                <td>
+                  <button onClick={() => handleDelete(meta.id)}>Eliminar</button>
+                  <button onClick={() => startEdit(meta)}>Editar</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <div className="no-metas">
+          No hay metas
+        </div>
+      )}
     </div>
   );
 }
